f211c632dc4f2fffd7ae86dae0277cd9d289f804
Adam Lukacovic <adam@adamlukacovic.sk>
2022-08-06 22:29:15 +0200

    Add date variable for title template

--- src/parsers/BilibiliParser.ts
+++ src/parsers/BilibiliParser.ts
@@ -35,7 +35,9 @@ class BilibiliParser extends Parser {
             .replace(/%videoId%/g, videoId)
             .replace(/%videoPlayer%/g, videoPlayer);
 
-        const fileNameTemplate = this.settings.bilibiliNoteTitle.replace(/%title%/g, videoTitle);
+        const fileNameTemplate = this.settings.bilibiliNoteTitle
+            .replace(/%title%/g, videoTitle)
+            .replace(/%date%/g, this.getFormattedDateForFilename());
         const fileName = `${fileNameTemplate}.md`;
         return new Note(fileName, content);
     }
